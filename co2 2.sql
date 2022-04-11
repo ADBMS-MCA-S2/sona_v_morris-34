007e5074cca044e4a24c61954c3829813d0d187b
sonavmorris <93874702+sonavmorris@users.noreply.github.com>
2022-04-11 16:13:28 +0530

    Update co2 2.sql

--- co2 2.sql
+++ co2 2.sql
@@ -9,3 +9,12 @@ insert into employee values(110,'ancy',22000);
 select emp_id,ename,salary,incentive(emp_id) from employee;
 
 
+CREATE DEFINER=`root`@`localhost` FUNCTION `incentive`(emp_id int(20)) RETURNS int(20)
+BEGIN
+declare i int(20);
+if(emp_id=110)
+then
+set i='3000';
+end if;
+RETURN i;
+END
