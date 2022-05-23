743bbca4b95e3d571bab234c590f9f3c9527ce05
sonavmorris <93874702+sonavmorris@users.noreply.github.com>
2022-05-23 15:08:30 +0530

    Update rahulmongo.sql

--- rahulmongo.sql
+++ rahulmongo.sql
@@ -5,3 +5,22 @@ db.employee_details.insert({"eid" : 102, "ename" : "arjun", "dept" : "sales", "d
 db.employee_details.insert({"eid" : 103, "ename" : "rahul", "dept" : "production", "desig" : "developer", "salary" : 66000, "address" : { "dno" : 304, "street" : "banglore", "locality" : "shivatemple", "city" : "banglore" }})
 db.employee_details.insert({"eid" : 104, "ename" : "maya", "dept" : "sales", "desig" : "hr", "salary" : 50000, "yoj" : 2001, "address" : { "dno" : 207, "street" : "manipur", "locality" : "balanagar", "city" : "hyderabad" }})
 db.employee_details.find()
+db.employee_details.find().pretty()
+
+
+
+
+
+
+
+
+
+//push keyword
+> db.employee_details.updateMany({"eid":101},{$push:{"project":10}})
+//pull 
+> db.employee_details.updateMany({"eid":101},{$pull:{"project":10}})
+//addToSet
+> db.employee_details.updateMany({"eid":101},{$addToSet:{"project":10}})
+//AddToSet array inside an array
+> db.employee_details.updateMany({"eid":101},{$addToSet:{"project":[20,30]}})
+//regular expression regex
